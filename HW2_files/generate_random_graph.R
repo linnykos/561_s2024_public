0b92ef054404daadffad6d6d80c444c011b571c0
linnykos <kzlin@uw.edu>
2024-04-08 10:55:10 -0400

    Pushing an update to the file

--- HW2_files/generate_random_graph.R
+++ HW2_files/generate_random_graph.R
@@ -1,69 +1,37 @@
-set.seed(0)
-res <- generate_random_graph(n = 20,
-                                 clique_fraction = 0.5,
-                                 density_low = 0.3)
-adj_mat <- res$adj_mat
-
-# Convert the matrix to a tibble (a type of data frame) for easier manipulation
-colnames(adj_mat) <- paste0("node:", 1:nrow(adj_mat))
-mat_df <- as_tibble(adj_mat)
-
-# Add row numbers as a new column, since gather() will melt all existing columns
-mat_df <- mat_df %>% mutate(Row = row_number())
-
-# Use gather() to convert from wide to long format
-mat_long <- mat_df %>%
-  gather(key = "Column", value = "Value", -Row) %>%
-  mutate(Column = as.numeric(gsub("node:", "", Column))) %>%
-  rename(X = Row, Y = Column)
-
-# Plot using ggplot
-ggplot(mat_long, aes(x = X, y = Y, fill = factor(Value))) +
-  geom_tile(color = "white") + # Use color to distinguish tiles
-  scale_fill_manual(values=c("0"="palegoldenrod", "1"="red")) + # Set colors
-  labs(fill = "Value") + # Label for the legend
-  xlab("X axis") + ylab("Y axis") + # Axis labels
-  theme_minimal() + # Use a minimal theme
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) # Improve axis text readability
-
-###############
-
-adj_mat <- res$adj_mat[res$rev_order, res$rev_order]
-# n <- nrow(adj_mat)
-# rownames(adj_mat) <- paste0("node:",1:n)
-# colnames(adj_mat) <- paste0("node:",1:n)
-
-# Convert the matrix to a tibble (a type of data frame) for easier manipulation
-colnames(adj_mat) <- paste0("node:", 1:nrow(adj_mat))
-mat_df <- as_tibble(adj_mat)
-
-# Add row numbers as a new column, since gather() will melt all existing columns
-mat_df <- mat_df %>% mutate(Row = row_number())
-
-# Use gather() to convert from wide to long format
-mat_long <- mat_df %>%
-  gather(key = "Column", value = "Value", -Row) %>%
-  mutate(Column = as.numeric(gsub("node:", "", Column))) %>%
-  rename(X = Row, Y = Column)
-
-# Plot using ggplot
-ggplot(mat_long, aes(x = X, y = Y, fill = factor(Value))) +
-  geom_tile(color = "white") + # Use color to distinguish tiles
-  scale_fill_manual(values=c("0"="palegoldenrod", "1"="red")) + # Set colors
-  labs(fill = "Value") + # Label for the legend
-  xlab("X axis") + ylab("Y axis") + # Axis labels
-  theme_minimal() + # Use a minimal theme
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) # Improve axis text readability
-
-
-#################
-
-# Dummy data
-x <- LETTERS[1:20]
-y <- paste0("var", seq(1,20))
-data <- expand.grid(X=x, Y=y)
-data$Z <- runif(400, 0, 5)
-
-# Heatmap 
-ggplot(data, aes(X, Y, fill= Z)) + 
-  geom_tile()
+generate_random_graph <- function(n,
+                                  clique_fraction = 0.2,
+                                  density_low = 0.1){
+  # check all the arguments are correct
+  stopifnot(n %% 1 == 0, n >= 0, 
+            clique_fraction >= 0, clique_fraction <= 1,
+            density_low >= 0, density_low <= 1)
+  
+  # generate an unsymmetric matrix
+  adj_mat <- matrix(sample(x = c(0,1),
+                           size = n^2,
+                           prob = c(1-density_low, density_low),
+                           replace = T), 
+                    nrow = n, ncol = n)
+  
+  # symmetrize the matrix
+  adj_mat <- adj_mat + t(adj_mat)
+  adj_mat[adj_mat > 0] <- 1
+  diag(adj_mat) <- 1
+  
+  # form the clique
+  clique_size <- ceiling(n * clique_fraction)
+  adj_mat[1:clique_size, 1:clique_size] <- 1
+  
+  # randomize the order of the nodes
+  sample_idx <- sample(1:n)
+  adj_mat <- adj_mat[sample_idx, sample_idx]
+  
+  # compute the appropriate reverse order
+  rev_order <- sapply(1:n, function(i){
+    which(sample_idx == i)
+  })
+  # to see what happens, try: adj_mat[rev_order, rev_order]
+  
+  list(adj_mat = adj_mat,
+       rev_order = rev_order)
+}
