56ed44ee43e752994258de89971281969e57c37f
linnykos <kzlin@uw.edu>
2024-04-11 08:02:34 -0700

    Pushing lecture 3

--- HW2_files/random_graph_functions.R
+++ HW2_files/random_graph_functions.R
@@ -1,39 +1,39 @@
 generate_random_graph <- function(n,
                                   clique_fraction = 0.2,
                                   density_low = 0.1){
-  # check all the arguments are correct
+  # Check all the arguments are correct
   stopifnot(n %% 1 == 0, n >= 0, 
             clique_fraction >= 0, clique_fraction <= 1,
             density_low >= 0, density_low <= 1)
   
-  # generate an unsymmetric matrix
+  # Generate an unsymmetric matrix
   adj_mat <- matrix(sample(x = c(0,1),
                            size = n^2,
                            prob = c(1-density_low, density_low),
                            replace = TRUE), 
                     nrow = n, ncol = n)
   
-  # symmetrize the matrix
+  # Symmetrize the matrix
   adj_mat <- adj_mat + t(adj_mat)
   adj_mat[adj_mat > 0] <- 1
   diag(adj_mat) <- 1
   
-  # form the clique
+  # Form the clique
   clique_size <- ceiling(n * clique_fraction)
   adj_mat[1:clique_size, 1:clique_size] <- 1
   
-  # randomize the order of the nodes
+  # Randomize the order of the nodes
   sample_idx <- sample(1:n)
   adj_mat <- adj_mat[sample_idx, sample_idx]
   
-  # compute the appropriate reverse order
+  # Compute the appropriate reverse order
   rev_order <- sapply(1:n, function(i){
     which(sample_idx == i)
   })
-  # to see what happens, try: adj_mat[rev_order, rev_order]
+  # To see what happens, try: adj_mat[rev_order, rev_order]
   
-  list(adj_mat = adj_mat,
-       rev_order = rev_order)
+  return(list(adj_mat = adj_mat,
+              rev_order = rev_order))
 }
 
 
@@ -53,5 +53,5 @@ pivot_longer_heatmap <- function(adj_mat){
     mutate(Column = as.numeric(gsub("node:", "", Column))) %>%
     rename(X = Row, Y = Column)
   
-  mat_long
+  return(mat_long)
 }
